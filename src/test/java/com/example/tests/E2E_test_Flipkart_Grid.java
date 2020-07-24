76026ea1715e744e40923d183f82b4aee55b05bb
Chiranjeevi <chiranjeevic26@gmail.com>
2020-07-24 12:30:34 +0530

    commit

--- src/test/java/com/example/tests/E2E_test_Flipkart_Grid.java
+++ src/test/java/com/example/tests/E2E_test_Flipkart_Grid.java
@@ -29,26 +29,28 @@ public class E2E_test_Flipkart_Grid extends Base {
 		System.out.println("Trying with :"+loginData.get("UserName"));
 		System.out.println(driver.getTitle());
 		
-		Base.wait2Seconds();
-		ln.userName().click();
-		ln.userName().sendKeys(loginData.get("UserName").get(0));
-		ln.password().click();
-		ln.password().sendKeys(loginData.get("Password").get(0));
-		ln.submit().click();
+		for(int i=0;i<=loginData.size();i++) {
+				Base.wait2Seconds();
+				ln.userName().click();
+				ln.userName().sendKeys(loginData.get("UserName").get(i));
+				ln.password().click();
+				ln.password().sendKeys(loginData.get("Password").get(i));
+				ln.submit().click();
 		
 		System.out.println("Login success");
 		Base.wait2Seconds();
 		
 		prd.productSearch().click();
-		prd.productSearch().sendKeys(loginData.get("productName").get(0));
+		prd.productSearch().sendKeys(loginData.get("productName").get(i));
 		prd.productSearch().sendKeys(Keys.ENTER);
 		
-		System.out.println("searchable product is :"+loginData.get("productName").get(0));
+		System.out.println("searchable product is :"+loginData.get("productName").get(i));
 		Base.wait2Seconds();
 		
 		prd.productToSelect().get(4).click();
 		String selectedProduct=prd.productToSelect().get(4).getText();
 		
+		Base.wait2Seconds();
 		for (String handle : driver.getWindowHandles()) {
 			 
 		    driver.switchTo().window(handle);
@@ -61,10 +63,19 @@ public class E2E_test_Flipkart_Grid extends Base {
 			System.out.println("product mismatch");
 		}
 		
-		
+		chk.customerName().sendKeys(loginData.get("CustomerName").get(i));
+		chk.customerPhoneNumber().sendKeys(loginData.get("PhoneNumber").get(i));
+		chk.pincode().sendKeys(loginData.get("Pincode").get(i));
+		chk.locality().sendKeys(loginData.get("Locality").get(i));
+		chk.addressArea().sendKeys(loginData.get("AddressArea").get(i));
+		chk.cityOrtownOrDistrict().sendKeys(loginData.get("cityOrTownOrDist").get(i));
+		chk.stateSelect().click();
+		Base.getValues(chk.stateTobeSelect(), "Karnataka");
 		
 
 	}
+		chk.clickOnUser().click();
+		
 
-	
+	}
 }
